09fbb8bfdb52c7eb4cc7401d32794f1cd8f89b8f
Rio Martinez <rio@riomartinez.com>
2019-10-10 18:17:33 -0500

    include ts(x) files in linting

--- .huskyrc.js
+++ .huskyrc.js
@@ -4,8 +4,8 @@ module.exports.hooks = {
 	'pre-commit': tasks([
 		'sort-package-json',
 		'git add package.json',
-		'pretty-quick --staged',
-		'pretty-quick --since $(git rev-list HEAD | tail -n 1) --bail',
+		'pretty-quick --pattern "**/*.*(js|ts|jsx|tsx|json)" --staged',
+		'pretty-quick --pattern "**/*.*(js|ts|jsx|tsx|json)" --since $(git rev-list HEAD | tail -n 1) --bail',
 		'git stash --keep-index',
 		'yarn test',
 		'git stash pop'
